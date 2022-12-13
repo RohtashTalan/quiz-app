7552086698d7ba7cb818eac0b5cf88c4a422c6cf
Rohtash Talan <rohtashtalancompany@gmail.com>
2022-12-13 15:06:23 +0530

    small update of removing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from "react";
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Homepage from "./components/Homepage";
-import QuizApp from "./components/QuizPage"; 
-import QuizResult from "./components/ResultPage";
-import Questions from "./components/data/question.json"
+import QuizApp from "./components/QuizPage";
 
 function App() {
   return (
