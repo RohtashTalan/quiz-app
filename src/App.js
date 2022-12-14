a2295812ad643c06c0948e65f228fe814bca0d04
Rohtash Talan <rohtashtalancompany@gmail.com>
2022-12-14 13:35:06 +0530

    removed routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from "react";
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Homepage from "./components/Homepage";
-import QuizApp from "./components/QuizPage";
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Homepage/>} />
-      <Route path='/quiz' element={<QuizApp/>} />
-    </Routes>
-    </BrowserRouter>
+    <Homepage/>
   );
 }
 
